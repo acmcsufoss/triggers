cf50359a76ceff007db6f18bdcde5ae4fa3247e7
Joel <100006388+joelrico@users.noreply.github.com>
2022-11-16 02:28:45 -0800

    Removed unused import from testing

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -1,6 +1,5 @@
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
-import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
