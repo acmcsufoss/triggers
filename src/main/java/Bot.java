155eec1f6cc32fedeea3158d684bb780aedd12a7
Joel <100006388+joelrico@users.noreply.github.com>
2022-11-16 01:38:37 -0800

    Created starter bot implementation

---
+++ src/main/java/Bot.java
@@ -0,0 +1,29 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+public class Bot {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Dotenv dotenv = Dotenv.configure()
+                .systemProperties()
+                .load();
+
+        String token = System.getProperty("DISCORD_TOKEN");
+
+        JDA jda = JDABuilder.createDefault(token)
+                .setStatus(OnlineStatus.ONLINE)
+                .enableCache(CacheFlag.VOICE_STATE)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .build()
+                .awaitReady();
+
+        System.out.println("Active bot: " + jda.getSelfUser().getName() + "#" + jda.getSelfUser().getDiscriminator());
+    }
+}
