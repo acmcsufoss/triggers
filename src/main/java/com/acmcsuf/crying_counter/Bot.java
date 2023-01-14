3cac1996888b1d7db930bd9fc545ec8248d624f9
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-14 01:26:03 -0800

    Removed dotenv import

--- src/main/java/com/acmcsuf/crying_counter/Bot.java
+++ src/main/java/com/acmcsuf/crying_counter/Bot.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
