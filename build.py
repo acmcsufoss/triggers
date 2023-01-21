94b8c8d3b643757993288f5db18817cf2956c7b4
jjoeldaniel <100006388+jjoeldaniel@users.noreply.github.com>
2023-01-21 14:01:18 -0800

    Added comments

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 import os
 
-version = None
-
+# Remove old jar
 try:
     for file in os.listdir("."):
         if file.startswith('triggers') and file.endswith('.jar'):
@@ -10,12 +9,17 @@ try:
 except OSError:
     pass
 
+# Build jar
 os.system('gradle build')
 
+version = None
+
+# Get version number and rename jar
 for file in os.listdir("."):
     if file.startswith('triggers') and file.endswith('.jar'):
         version = file.replace('triggers-', '').replace('.jar', '')
         os.system(f'mv {file} triggers.jar')
         break
 
+# Build Docker image with tag
 os.system(f'docker build -t triggers:{version} .')
