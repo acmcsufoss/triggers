8a63bb6be6e2476e6e62c53e66296cec3172195d
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-06 02:55:39 -0800

    Added more TODOs, added basic sync

--- src/main/java/com/acmcsuf/triggers/BotController.java
+++ src/main/java/com/acmcsuf/triggers/BotController.java
@@ -1,4 +1,6 @@
 package com.acmcsuf.triggers;
+import java.sql.SQLException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +19,17 @@ public class BotController {
     @PostMapping("/api/sync")
     public ResponseEntity<String> sync(@RequestParam("user_id") String userId) {
 
-        // TODO: Add sync logic here
+        // TODO: Add authentication process (JWT?)
+
+        // TODO: Add validation for user_id
+
+        // Sync data from database
+        try {
+            Database.syncUserData(userId, Trigger.triggerMap, Trigger.triggerToggle);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error syncing data for user_id: " + userId, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
         return new ResponseEntity<>("Data synced successfully for user_id: " + userId, HttpStatus.OK);
     }
