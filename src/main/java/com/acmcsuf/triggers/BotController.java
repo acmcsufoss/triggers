186ad998360b8aec41a5c487de9c3536f5998af6
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-06 02:20:06 -0800

    Added sync endpoint

--- src/main/java/com/acmcsuf/triggers/BotController.java
+++ src/main/java/com/acmcsuf/triggers/BotController.java
@@ -1,5 +1,9 @@
 package com.acmcsuf.triggers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +12,14 @@ public class BotController {
     @GetMapping("/api/ping")
     public String ping() {
         return "Hello, world!";
+    }
+
+    @PostMapping("/api/sync")
+    public ResponseEntity<String> sync(@RequestParam("user_id") String userId) {
+
+        // TODO: Add sync logic here
+
+        return new ResponseEntity<>("Data synced successfully for user_id: " + userId, HttpStatus.OK);
     }
     
 }
